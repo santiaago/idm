a5cbaa4470458636d846ef23533a750fb435ecfe
santiaago <santiago.arias@red-gate.com>
2015-04-13 13:47:12 +0100

    Add parser type

--- apl.go
+++ apl.go
@@ -128,6 +128,21 @@ func (s *Scanner) scanIdentifier() (t Token, lit string) {
 	return Identifier, buf.String()
 }
 
+// Parser represents a parser
+type Parser struct {
+	s   *Scanner
+	buf struct {
+		t   Token  // last read token
+		lit string // last read literal
+		n   int    // buffer size (max=1)
+	}
+}
+
+// NewParser returns a new instance of Parser.
+func NewParser(r io.Reader) *Parser {
+	return &Parser{s: NewScanner(r)}
+}
+
 func main() {
 	fmt.Println("hello, world")
 }
