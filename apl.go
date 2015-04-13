9d6451691a91e6f06ec4399bab069b991ac911da
santiaago <santiago.arias@red-gate.com>
2015-04-13 13:44:08 +0100

    Add scanner methods

--- apl.go
+++ apl.go
@@ -51,6 +51,80 @@ func NewScanner(r io.Reader) *Scanner {
 	return &Scanner{r: bufio.NewReader(r)}
 }
 
+// read reads the next rune from the bufferred reader.
+// Returns the rune(0) if an error occurs (or io.EOF is returned)
+func (s *Scanner) read() rune {
+	r, _, err := s.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return r
+}
+
+func (s *Scanner) unread() { _ = s.r.UnreadRune() }
+
+// Scan retusn the next token and literal value.
+func (s *Scanner) Scan() (t Token, lit string) {
+	// read the next rune.
+	r := s.read()
+
+	// if we see whitespace then consume all contiguous whitespace.
+	// if we see a letter then consume as an identifier keyword word.
+	if isWhitespace(r) {
+		s.unread()
+		return s.scanWhitespace()
+	} else if isLetter(r) {
+		s.unread()
+		return s.scanIdentifier()
+	}
+
+	switch r {
+	case eof:
+		return EOF, ""
+	case '*':
+		return Asterix, string(r)
+	}
+	return Error, string(r)
+}
+
+// scanWhitespace consumes the current rune and all contiguous whitespace.
+func (s *Scanner) scanWhitespace() (t Token, lit string) {
+	// Create a buffer and read the current character into it.
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	// Read every subsequent whitespace character into the bufer.
+	// non whitespace characters and EOF will cause the loop to exit.
+	for {
+		if r := s.read(); r == eof {
+			break
+		} else if !isWhitespace(r) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(r)
+		}
+	}
+	return Space, buf.String()
+}
+
+func (s *Scanner) scanIdentifier() (t Token, lit string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	for {
+		if r := s.read(); r == eof {
+			break
+		} else if !isLetter(r) && !isDigit(r) && r != '_' {
+			s.unread()
+			break
+		} else {
+			_, _ = buf.WriteRune(r)
+		}
+	}
+	return Identifier, buf.String()
+}
+
 func main() {
 	fmt.Println("hello, world")
 }
