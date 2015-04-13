8ee0282e8fa390fe110dd6112138c6cecd466535
santiaago <santiago.arias@red-gate.com>
2015-04-13 16:49:10 +0100

    Add Parser scan test

---
+++ parser_test.go
@@ -0,0 +1,44 @@
+package idm
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestParserScan(t *testing.T) {
+	var tests = []struct {
+		s    string
+		stmt *Statement
+		err  string
+	}{
+		{
+			s: `a = b`,
+			stmt: &Statement{
+				Left:  "a",
+				Right: "b",
+			},
+		},
+		{
+			s:   `a`,
+			err: `found "", expected '='`,
+		},
+	}
+
+	for i, tt := range tests {
+		stmt, err := NewParser(strings.NewReader(tt.s)).Parse()
+		if !strings.Contains(errstring(err), tt.err) {
+			t.Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
+		} else if tt.err == "" && !reflect.DeepEqual(tt.stmt, stmt) {
+			t.Errorf("%d. %q\n\nstmt mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, tt.s, tt.stmt, stmt)
+		}
+	}
+}
+
+// errstring returns the string representation of an error.
+func errstring(err error) string {
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
