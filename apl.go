9074f97ea612ab1f01c38dbffde936d9c5f1a5f0
santiaago <santiago.arias@red-gate.com>
2015-04-13 13:13:46 +0100

    add tokens

--- apl.go
+++ apl.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+// Token identifies the type of lex items.
+type Token int
+
+const (
+	// EOF represents the end of file
+	EOF Token = iota
+	// Error represents an error
+	Error
+	// Assign represents the assignment '='
+	Assign
+	// Number represents a simple number
+	Number
+	// Operator an operator such as '+' '-' '*'
+	Operator
+	// Space represents space separation between tokens
+	Space
+	// Identifier represent an identifier such as a var name
+)
+
 func main() {
 	fmt.Println("hello, world")
 }
