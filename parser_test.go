dd9ea157da9071c6277c1c9d9f79ac967fd46e0c
santiaago <santiago.arias@red-gate.com>
2015-04-14 15:53:47 +0100

    Add + test

--- parser_test.go
+++ parser_test.go
@@ -20,6 +20,14 @@ func TestParser_Scan(t *testing.T) {
 			},
 		},
 		{
+			s: `1 + 2`,
+			expr: Binary{
+				Left:     Int(1),
+				Right:    Int(2),
+				Operator: "+",
+			},
+		},
+		{
 			s:   `a`,
 			err: `found "", expected '='`,
 		},
